a76eb8a23ecc2da12cde1dac489b2d30d08373e8
zhaoxiaochun-art <337416641@qq.com>
2021-01-21 17:45:52 +0800

    update

--- Device360/Device360.cpp
+++ Device360/Device360.cpp
@@ -1,68 +1,74 @@
 #include "Device360.h"
 
 Device360::Device360(QWidget *parent)
-    : QMainWindow(parent)
+	: QMainWindow(parent)
 {
-    ui.setupUi(this); 
+	ui.setupUi(this);
 	setWindowFlags(Qt::FramelessWindowHint);//无边框 
 
-	QRegExp regx("[a-zA-Z0-9_]+$");//正则表达式QRegExp,只允许输入中文、数字、字母、下划线以及空格,[\u4e00 - \u9fa5a - zA - Z0 - 9_] + $
-	ui.lE_PN->setValidator(new QRegExpValidator(regx, this));
-
-	ui.cB_RunSpeed->setVisible(false);	
-	ui.cB_RunSpeed->move(372, 7);
-	connect(ui.lE_RunSpeed, SIGNAL(POPUPKEYBOARD()), this, SLOT(changeRunSpeed()));
-	connect(ui.cB_RunSpeed, QOverload<const QString&>::of(&QComboBox::activated),
-		[=](const QString& text) {
-		QString str_Before = ui.lE_RunSpeed->text();
-		if (text != QString::fromLocal8Bit("取消"))
-		{
-			if (text == str_Before)
-			{
-				QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("速度未变化提示。"));
-			}
-			else if (QMessageBox::Yes == QMessageBox::question(nullptr,QString::fromLocal8Bit("确认框"),QString::fromLocal8Bit("改变速度吗？"),QMessageBox::Yes|QMessageBox::No))
-			{
-				ui.lE_RunSpeed->setText(text);
-				QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("设置PLC的系统运行速度并显示"));
-			}
-		}
-		else
-		{
-			ui.lE_RunSpeed->setText(str_Before);
-		}
-	});
+	m_ProgramDlg = new ProgramSet();
 }
-
 #pragma region msgbox
 void Device360::on_Button_Clean_toggled(bool checked)
 {
 	if (checked)
 	{
-		QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("设备因故障导致在停机时,设备上可能还有残余的胶囊。\n该按钮在停机状态下按下时会将胶囊都排入废品盒。同时滚轮会持续旋转，方便进行擦拭。再次按下后停止。"));
+		QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("Checkable 设备因故障导致在停机时,设备上可能还有残余的胶囊。\n该按钮在停机状态下按下时会将胶囊都排入废品盒。同时滚轮会持续旋转，方便进行擦拭。再次按下后停止。"));
 	}
 	else
 	{
 		QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("停止清洁功能。"));
 	}
 }
+void Device360::on_Button_Start_toggled(bool checked)
+{
+	if (checked)
+	{
+		QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("Checkable 启动检测，再次按下停止检测"));
+	}
+	else
+	{
+		QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("停止检测"));
+	}
+}
+void Device360::on_Button_AlarmReset_clicked()
+{
+		QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("系统故障清除后，点击该按钮进行故障复位，如果设备不在原点，还需要寻参"));
+
+}
+void Device360::on_Button_CameraSet_clicked()
+{
+	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("打开参数设置界面"));
+	m_ProgramDlg->show();
+}
+void Device360::on_Button_Log_clicked()
+{
+	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("打开日志查询界面"));
+
+}
+
+void Device360::on_Button_DataOut_clicked()
+{
+	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("打开数据导出日志"));
+
+}
+void Device360::on_Button_CountReset_clicked()
+{
+	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("将运行统计tab页表格中的数据清零 PLC数据tab页的数值清零"));
+
+}
 void Device360::on_Button_Exit_clicked()
 {
 	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("退出系统"));
 	close();
 }
-void Device360::on_Button_CountReset_clicked()
-{
-	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("退出系统"));
-}
-void Device360::changeRunSpeed()
-{
-	ui.lE_RunSpeed->clearFocus();
-	ui.cB_RunSpeed->showPopup();
-}
 
 void Device360::on_lE_PN_returnPressed()
 {
 	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("改变生产批号"));
+}
+void Device360::on_lE_RunSpeed_returnPressed()
+{
+	QMessageBox::about(nullptr, QString::fromLocal8Bit("功能"), QString::fromLocal8Bit("改变系统速度：10-120rpm"));
 }
 #pragma endregion
--- Device360/Device360.h
+++ Device360/Device360.h
@@ -3,6 +3,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QMessageBox>
 #include "ui_Device360.h"
+#include "ProgramSet.h"
 
 class Device360 : public QMainWindow
 {
@@ -12,11 +13,17 @@ public:
     Device360(QWidget *parent = Q_NULLPTR);
 
 private:
-    Ui::Device360Class ui;
+	Ui::Device360Class ui;
+	ProgramSet *m_ProgramDlg;
 public slots:
 	void on_Button_Clean_toggled(bool checked);
-	void on_Button_Exit_clicked();
+	void on_Button_Start_toggled(bool checked);
+	void on_Button_AlarmReset_clicked();
+	void on_Button_CameraSet_clicked();
+	void on_Button_Log_clicked();
+	void on_Button_DataOut_clicked();
 	void on_Button_CountReset_clicked();
-	void changeRunSpeed();
+	void on_Button_Exit_clicked();
 	void on_lE_PN_returnPressed();
+	void on_lE_RunSpeed_returnPressed();
 };
